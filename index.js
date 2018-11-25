233b70d21bf8a7f65294eecd96ba606a0629f51d
Mitsui Yasuyuki <mitsui.y.k@gmail.com>
2018-11-25 21:11:31 +0900

    expressで起動するように修正

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require('path');
+
+server.listen(8080);
+app.use('/css', express.static(path.join(__dirname, 'client', 'dist', 'css')));
+app.use('/js', express.static(path.join(__dirname, 'client', 'dist', 'js')));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/client/dist/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
